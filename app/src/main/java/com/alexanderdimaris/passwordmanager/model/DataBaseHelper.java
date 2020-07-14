64103263a06d0f9f3f2ddf8f581256e631465b20
Stella-31 <alexander.dimaris@pm.me>
2020-07-14 09:37:59 -0500

    Added Data Model:
    - added POJO PassObj class to store passwords
    - added a DataBaseHelper class

---
+++ app/src/main/java/com/alexanderdimaris/passwordmanager/model/DataBaseHelper.java
@@ -0,0 +1,99 @@
+package com.alexanderdimaris.passwordmanager.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String PASSWORD_TABLE = "PASSWORD_TABLE";
+    private static final String COLUMN_TITLE_NAME = "COLUMN_TITLE_NAME";
+    private static final String COLUMN_USERNAME = "COLUMN_USERNAME";
+    private static final String COLUMN_PASSWORD_TEXT = "COLUMN_PASSWORD TEXT";
+    private static final String COLUMN_COMMENTS = "COLUMN_COMMENTS";
+    private static final String COLUMN_ID = "ID";
+
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "password.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement =   "CREATE TABLE " + PASSWORD_TABLE
+                + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_TITLE_NAME + " TEXT, "
+                + COLUMN_USERNAME + " TEXT, "
+                + COLUMN_PASSWORD_TEXT + ", "
+                + COLUMN_COMMENTS + " TEXT)";
+        db.execSQL(createTableStatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // intentionally left blank
+    }
+
+    public boolean addOne(PassObj passwordObject) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE_NAME, passwordObject.getTitle());
+        cv.put(COLUMN_USERNAME, passwordObject.getUsername());
+        cv.put(COLUMN_PASSWORD_TEXT, passwordObject.getPassword());
+        cv.put(COLUMN_COMMENTS, passwordObject.getComments());
+
+        long insert = db.insert(PASSWORD_TABLE, null, cv);
+        if(insert == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean deleteOne(PassObj passwordObject) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String queryString = "DELETE FROM" + PASSWORD_TABLE + " WHERE " + COLUMN_ID + " = " + passwordObject.getId();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        boolean success = cursor.moveToFirst();
+        // TODO determine if I should close here
+//        cursor.close();
+//        db.close();
+
+        if(success) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ArrayList<PassObj> getAll() {
+        ArrayList<PassObj> returnList = new ArrayList<>();
+        String queryString = "SELECT * FROM " + PASSWORD_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                int passwordId = cursor.getInt(0);
+                String passwordTitle = cursor.getString(1);
+                String username = cursor.getString(2);
+                String password = cursor.getString(3);
+                String comments = cursor.getString(4);
+
+                PassObj newPassword = new PassObj(passwordId, passwordTitle, username, password, comments);
+                returnList.add(newPassword);
+            } while(cursor.moveToNext());
+        } else {
+            // intentionally left blank
+        }
+        cursor.close();
+        db.close();
+        return returnList;
+    }
+}
---
+++ app/src/main/java/com/alexanderdimaris/passwordmanager/model/PassObj.java
@@ -0,0 +1,63 @@
+package com.alexanderdimaris.passwordmanager.model;
+
+public class PassObj {
+
+    private int id;
+    private String title;
+    private String username;
+    private String password;
+    private String comments;
+
+    public PassObj (int id, String title, String username, String password, String comments) {
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.password = password;
+        this.comments = comments;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String prettyPrint() {
+        return "Title=" + this.getTitle() + " Username=" + this.getUsername() + " Password=" + this.getPassword() + " Comments=" + this.getComments();
+    }
+
+}
