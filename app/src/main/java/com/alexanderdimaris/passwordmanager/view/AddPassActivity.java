b7a7fe98e1ae27908c1bb42192492f6431f6e2c3
Stella-31 <alexander.dimaris@pm.me>
2020-07-14 12:09:02 -0500

    Added the UI layer for randomly generating a new password

--- app/src/main/java/com/alexanderdimaris/passwordmanager/view/AddPassActivity.java
+++ app/src/main/java/com/alexanderdimaris/passwordmanager/view/AddPassActivity.java
@@ -2,9 +2,12 @@ package com.alexanderdimaris.passwordmanager.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.alexanderdimaris.passwordmanager.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddPassActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class AddPassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pass);
+
+        TextInputLayout textInputLayout = findViewById(R.id.activity_add_pass_password_edit_text);
+        textInputLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PassGeneratorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
