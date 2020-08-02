99725b64bd92871f95b52e13be56704410597864
Stella-31 <alexander.dimaris@pm.me>
2020-08-02 13:02:21 -0500

    Added snackbar to activity_login.xml

--- app/src/main/java/com/alexanderdimaris/passwordmanager/LoginActivity.java
+++ app/src/main/java/com/alexanderdimaris/passwordmanager/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.alexanderdimaris.passwordmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,12 +11,15 @@ import android.widget.Toast;
 
 import com.alexanderdimaris.passwordmanager.view.PassListActivity;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private MaterialButton btLogin;
     private TextInputEditText etUsername, etPassword;
+    private CoordinatorLayout snackBar;
 
     private final static String USERNAME = "username";
     private final static String PASSWORD = "password";
@@ -33,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.login_password_et);
         btLogin = findViewById(R.id.login_button);
         btLogin.setOnClickListener(v -> login());
+        snackBar = findViewById(R.id.login_snackbar);
     }
 
     private void login() {
@@ -40,7 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         String password = etPassword.getText().toString();
 
         if(username.equals("") || password.equals("")) {
-            Toast.makeText(this, "Required filed left blank", Toast.LENGTH_LONG).show();
+            snackBar("Required field left blank");
         } else {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
             SharedPreferences.Editor prefsEdit = prefs.edit();
@@ -56,13 +61,18 @@ public class LoginActivity extends AppCompatActivity {
                 launchPassListActivity();
             } else {
                 if(username.equals(originalUsername) && password.equals(originalPassword)) {
-                    prefsEdit.putBoolean("isLogged", true);
                     launchPassListActivity();
                 } else {
-                    Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_LONG).show(); // TODO snackbar
+                    snackBar("Incorrect username or password");
                 }
             }
         }
+    }
+
+    public void snackBar(String string) {
+        Snackbar.make(snackBar, string, Snackbar.LENGTH_LONG)
+                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
+                .show();
     }
 
     public void launchPassListActivity() {
